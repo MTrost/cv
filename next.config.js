7dec6a61f6ce9f49e85660855a007757180f33b9
Damian Frizzi <damian.f@v-designs.ch>
2018-10-31 17:18:48 +0100

    Correct assetPrefix in production for github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const reponame = 'cv-generator'
 
 module.exports = withTypescript(
   withCSS({
     cssModules: true,
+    assetPrefix: process.env.NODE_ENV === 'production' ? `/${reponame}` : '',
     webpack(config, options) {
       // Do not run type checking twice:
       if (options.isServer) {
