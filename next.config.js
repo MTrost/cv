ec3342af0dcddf14446338480900a01a6fab0058
Damian Frizzi <damian.f@v-designs.ch>
2018-11-13 20:40:00 +0100

    Don't use hashed classnames in dev mode

--- next.config.js
+++ next.config.js
@@ -1,11 +1,30 @@
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
+const { withPlugins } = require('next-compose-plugins')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 const reponame = 'cv'
 
-module.exports = withTypescript(
-  withCSS({
-    cssModules: true,
+module.exports = withPlugins(
+  [
+    withTypescript,
+    [
+      withCSS,
+      {
+        cssModules: true,
+        cssLoaderOptions: {
+          localIdentName: '[local]--[hash:base64:5]'
+        },
+        [PHASE_PRODUCTION_BUILD]: {
+          cssLoaderOptions: {
+            importLoaders: 1,
+            localIdentName: '[hash:base64:4]'
+          }
+        }
+      }
+    ]
+  ],
+  {
     assetPrefix: process.env.NODE_ENV === 'production' ? `/${reponame}` : '',
     webpack(config, options) {
       // Do not run type checking twice:
@@ -15,5 +34,5 @@ module.exports = withTypescript(
 
       return config
     }
-  })
+  }
 )
